f749bad16919089fa4df7904cfdc9924642f52f3
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-01-11 11:57:38 +0000

    保存機能を追加, ストロングパラメータ,

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,6 +4,15 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+ # 以下を追加
+  def create
+   # 1.＆ 2.データを受け取り漆器登録するためのインスタンス作成
+   list = List.new(list_params)
+   # 3.　データをデータベースに保存するためのsaveメソッド実行
+   list.save
+   # 4. トップ画面へリダイレクト
+   redirect_to '/top'
+  end
   def index
   end
 
@@ -12,4 +21,10 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+   params.require(:list).permit(:title, :body)
+  end 
 end
