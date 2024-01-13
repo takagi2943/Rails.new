9f65126ed0e47b7f88c682bd70cdf1fcb2374e33
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-01-13 01:44:57 +0000

    バリデーション【バリデーションを設定】

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,14 +6,12 @@ class ListsController < ApplicationController
 
  # 以下を追加
   def create
-   # 1.＆ 2.データを受け取り漆器登録するためのインスタンス作成
-   list = List.new(list_params)
-   # 3.　データをデータベースに保存するためのsaveメソッド実行
-   list.save
-   # 4. トップ画面へリダイレクト
-   # redirect_to '/top'を削除して、以下コードに変更
-   # 詳細画面はリダイレクト
-   redirect_to list_path(list.id)
+   @list =List.new(list_params)
+   if @list.save
+     redirect_to list_path(@list.id)
+   else
+    render :new
+   end
   end
 
   def index
